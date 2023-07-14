da52204fa6154f8c8f761ffa0a90d2bf4c527c87
Arsalan Kazmi <sonicspeed848@gmail.com>
2023-07-14 03:36:09 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='shifter',
+    version='1.0.0',
+    author='Arsalan Kazmi',
+    author_email='sonicspeed848@gmail.com',
+    description='A module for shifting values to different destinations.',
+    py_modules=['shift'],
+    python_requires='>=3.6',
+)
