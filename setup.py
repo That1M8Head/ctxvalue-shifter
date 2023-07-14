bd52583232e0ce3a52a6f464ee46908b31f8e1ea
Arsalan Kazmi <sonicspeed848@gmail.com>
2023-07-14 10:41:24 +0100

    Bump to v1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='shifter',
-    version='1.0.4',
+    version='1.0.5',
     author='Arsalan Kazmi',
     author_email='sonicspeed848@gmail.com',
     description='A module for shifting values to different destinations.',
